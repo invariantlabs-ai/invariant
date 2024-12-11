81547a0d78a13951a6e415b9cd839d0ab68c6645
Hemang <hemang@invariantlabs.ai>
2024-12-11 14:28:21 +0100

    Fix case in traverse_dot_path where the value is None when traversing.

--- testing/tests/test_trace.py
+++ testing/tests/test_trace.py
@@ -1,6 +1,7 @@
 import pytest
 
-from invariant.custom_types.trace import Trace
+
+from invariant.custom_types.trace import Trace, traverse_dot_path
 
 
 @pytest.fixture(name="message_list")
@@ -20,6 +21,14 @@ def fixture_message_list():
         ]
     )
     return trace.messages()
+
+
+@pytest.fixture(name="dict_traversal")
+def fixture_for_dict_traversal():
+    return {
+        "user": {"name": "Alice", "details": {"age": 30, "email": "alice@example.com"}},
+        "function": {"tool_calls": {"name": "example_tool", "status": "success"}},
+    }
 
 
 def test_trace_iterable(message_list: Trace):
@@ -58,3 +67,28 @@ def test_trace_messages_with_index():
         trace.messages(-10)
         trace.messages(-5)
         trace.messages(5)
+
+
+def test_traverse_dot_path_found_match(dict_traversal):
+    value, prefix_added = traverse_dot_path(dict_traversal, "user.details.email")
+    assert value == "alice@example.com"
+    assert not prefix_added
+
+
+def test_traverse_dot_path_no_found_match_with_function_prefix(dict_traversal):
+    value, prefix_added = traverse_dot_path(dict_traversal, "user.details.phone")
+    assert value is None
+    assert prefix_added  # Because function prefix was attempted.
+
+
+def test_traverse_dot_path_found_match_with_function_prefix(dict_traversal):
+    value, prefix_added = traverse_dot_path(dict_traversal, "tool_calls.name")
+    assert value == "example_tool"
+    assert prefix_added  # Because function prefix was attempted.
+
+
+def test_traverse_dot_path_testing_none():
+    dict1 = {"user": None}
+    value, prefix_added = traverse_dot_path(dict1, "user.details")
+    assert value is None
+    assert prefix_added  # Because function prefix was attempted.
